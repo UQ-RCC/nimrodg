8c690fe511b171c47b1639eb8aed4e974b51f5c1
Zane van Iperen <z.vaniperen@uq.edu.au>
2019-08-21 17:44:56 +1000

    nimrodg-resources: more jclouds experimentation

--- nimrodg-resources/src/main/java/au/edu/uq/rcc/nimrodg/resource/cloud/JcloudsActuator.java
+++ nimrodg-resources/src/main/java/au/edu/uq/rcc/nimrodg/resource/cloud/JcloudsActuator.java
@@ -27,56 +27,37 @@ import au.edu.uq.rcc.nimrodg.api.NimrodURI;
 import au.edu.uq.rcc.nimrodg.api.Resource;
 import au.edu.uq.rcc.nimrodg.api.ResourceFullException;
 import au.edu.uq.rcc.nimrodg.api.utils.NimrodUtils;
-import au.edu.uq.rcc.nimrodg.api.utils.ResourceUtils;
 import au.edu.uq.rcc.nimrodg.resource.act.ActuatorUtils;
-import au.edu.uq.rcc.nimrodg.resource.act.RemoteActuator;
-import au.edu.uq.rcc.nimrodg.resource.ssh.OpenSSHClient;
-import au.edu.uq.rcc.nimrodg.resource.ssh.RemoteShell;
-import au.edu.uq.rcc.nimrodg.resource.ssh.SSHClient;
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.URI;
 import java.security.KeyPair;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import java.security.cert.Certificate;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.PriorityQueue;
 import java.util.Properties;
-import java.util.Queue;
 import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
+import javax.ws.rs.core.UriBuilder;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jclouds.ContextBuilder;
 import org.jclouds.compute.ComputeService;
 import org.jclouds.compute.ComputeServiceContext;
 import org.jclouds.compute.RunNodesException;
-import org.jclouds.compute.domain.ComputeMetadata;
 import org.jclouds.compute.domain.Hardware;
-import org.jclouds.compute.domain.Image;
 import org.jclouds.compute.domain.NodeMetadata;
-import org.jclouds.compute.domain.OsFamily;
 import org.jclouds.compute.domain.Template;
 import org.jclouds.compute.options.TemplateOptions;
-import org.jclouds.domain.Location;
 import org.jclouds.domain.LoginCredentials;
 import org.jclouds.http.Uris;
-import org.jclouds.openstack.keystone.config.KeystoneProperties;
-import org.jclouds.openstack.nova.v2_0.NovaApi;
 import org.jclouds.openstack.nova.v2_0.compute.options.NovaTemplateOptions;
-import org.jclouds.openstack.nova.v2_0.features.ServerApi;
-import org.jclouds.ssh.SshClient;
 import org.jclouds.sshj.config.SshjSshClientModule;
 
 public class JcloudsActuator implements Actuator {
@@ -101,47 +82,46 @@ public class JcloudsActuator implements Actuator {
 
 		public final NodeMetadata node;
 		public final Set<UUID> agents;
-		public final Optional<KeyPair> keyPair;
-		public final Optional<String> username;
-		public final Optional<String> password;
-		public final URI uri;
+
+		private boolean isConfigured;
+		private String username;
+		private Optional<String> password;
+		private Optional<KeyPair> keyPair;
+		private List<URI> uris;
 
 		public NodeInfo(NodeMetadata node) {
 			this.node = node;
 			this.agents = new HashSet<>();
-			Optional<LoginCredentials> creds = Optional.ofNullable(node.getCredentials());
-			this.keyPair = creds.flatMap(c -> Optional.ofNullable(c.getOptionalPrivateKey().orNull()))
-					.map(s -> {
-						try {
-							return ActuatorUtils.readPEMKey(s);
-						} catch(IOException e) {
-							// FIXME: This will abort everything
-							throw new UncheckedIOException(e);
-						}
-					});
-
-			this.username = creds.map(c -> c.getUser());
-			this.password = creds.flatMap(c -> Optional.ofNullable(c.getOptionalPassword().orNull()));
-			{
-				StringBuilder sb = new StringBuilder("ssh://");
-				username.ifPresent(u -> {
-					sb.append(u);
-					password.ifPresent(p -> {
-						sb.append(":");
-						sb.append(p);
-					});
-					sb.append("@");
-				});
-
-				// TODO: This properly
-				sb.append(node.getPublicAddresses().stream().findFirst().get());
-
-				sb.append(":");
-				sb.append(node.getLoginPort());
-				this.uri = URI.create(sb.toString());
-			}
+			this.isConfigured = false;
+			this.username = null;
+			this.password = Optional.empty();
+			this.keyPair = Optional.empty();
+			this.uris = List.of();
 		}
 
+		void configure(String username, Optional<String> password, Optional<KeyPair> keyPair) {
+			if(isConfigured) {
+				throw new IllegalStateException();
+			}
+
+			String userInfo = password.map(p -> String.format("%s:%s", username, p)).orElse(username);
+
+			this.username = username;
+			this.password = password;
+			this.keyPair = keyPair;
+			this.uris = node.getPublicAddresses().stream()
+					.map(addr -> UriBuilder.fromUri("")
+					.scheme("ssh")
+					.host(addr)
+					.userInfo(userInfo)
+					.port(node.getLoginPort())
+					.build()).collect(Collectors.toList());
+			isConfigured = true;
+		}
+
+		public boolean isConfigured() {
+			return this.isConfigured;
+		}
 	}
 
 	public static class CloudConfig {
@@ -242,11 +222,12 @@ public class JcloudsActuator implements Actuator {
 		}
 	}
 
-	private void launchNodes(int num, Set<NodeMetadata> good, Set<NodeMetadata> bad) {
+	private void launchNodes(int num, Set<NodeMetadata> good, Map<NodeMetadata, Throwable> bad) {
 		try {
 			good.addAll(compute.createNodesInGroup(groupName, num, template));
 		} catch(RunNodesException e) {
 			good.addAll(e.getSuccessfulNodes());
+
 			//e.g
 			// TODO: This
 		}
@@ -256,8 +237,6 @@ public class JcloudsActuator implements Actuator {
 	public LaunchResult[] launchAgents(UUID[] uuids) throws IOException {
 		LaunchResult[] results = new LaunchResult[uuids.length];
 		LaunchResult failedResult = new LaunchResult(null, new ResourceFullException(node));
-
-		Set<NodeMetadata> bad = new HashSet<>();
 
 		/* See if there's any space left on our existing nodes. */
 		LinkedHashSet<NodeInfo> good = nodes.values().stream()
@@ -275,20 +254,59 @@ public class JcloudsActuator implements Actuator {
 				+ ((uuids.length - nLaunchable) % agentsPerNode > 0 ? 1 : 0);
 
 		// FIXME: I don't like this
+		Map<NodeMetadata, Throwable> bad = new HashMap<>();
 		Set<NodeMetadata> _good = new HashSet<>();
 		if(nLaunchable < uuids.length) {
 			launchNodes(numRequired, _good, bad);
 		}
 
-		_good.stream()
-				.map(n -> NimrodUtils.getOrAddLazy(nodes, n, nn -> new NodeInfo(nn)))
-				.forEach(good::add);
+		/*
+		 * Add all spawned nodes now, so we still know about them if something throws.
+		 * I'm not confident enough in the validation code below to not do this.
+		 */
+		_good.forEach(n -> nodes.put(n, new NodeInfo(n)));
 
-//		/*
-//		 * NB: `good` contains a list of nodes, sorted by the number of agents in descending order.
-//		 * Any new nodes are stored in the global list now, so they can be accounted for later incase
-//		 * the actual agent launches fails for some reason.
-//		 */
+		/* Validate the credentials for each node. */
+		for(NodeMetadata n : _good) {
+			final LoginCredentials creds = n.getCredentials();
+			if(creds == null) {
+				bad.put(n, new RuntimeException("No credentials for node"));
+				continue;
+			}
+
+			if(!creds.getOptionalPassword().isPresent() && !creds.getOptionalPrivateKey().isPresent()) {
+				bad.put(n, new IllegalStateException("no password or private key"));
+				continue;
+			}
+
+			Optional<String> _privKey = Optional.ofNullable(creds.getOptionalPrivateKey().orNull());
+			Optional<KeyPair> keyPair = Optional.empty();
+			if(_privKey.isPresent()) {
+				try {
+					keyPair = Optional.of(ActuatorUtils.readPEMKey(_privKey.get()));
+				} catch(IOException e) {
+					bad.put(n, e);
+					continue;
+				}
+			}
+
+			NodeInfo ni = nodes.get(n);
+			ni.configure(
+					creds.getUser(),
+					Optional.ofNullable(creds.getOptionalPassword().orNull()),
+					keyPair
+			);
+			good.add(ni);
+		}
+
+		/* FIXME: Untested */
+		bad.keySet().forEach(n -> compute.destroyNode(n.getId()));
+
+		/*
+		 * NB: `good` contains a list of nodes, sorted by the number of agents in descending order.
+		 * Any new nodes are stored in the global list now, so they can be accounted for later incase
+		 * the actual agent launches fails for some reason.
+		 */
 //
 //		{
 //			class Tmp {
@@ -342,23 +360,27 @@ public class JcloudsActuator implements Actuator {
 		return false;
 	}
 
-	private RemoteShell makeSsh(NodeInfo n) throws IOException {
-		return new SSHClient(
-				n.uri,
-				new PublicKey[0], n.keyPair.get());
-	}
-
+//	private RemoteShell makeSsh(NodeInfo n) throws IOException {
+//		return new SSHClient(
+//				n.uri,
+//				new PublicKey[0], n.keyPair.get());
+//	}
 	@Override
 	public void close() throws IOException {
 		for(NodeInfo ni : nodes.values()) {
+			/* Ignore nodes that failed to set up and are still here for some reason. */
+			if(!ni.isConfigured) {
+				continue;
+			}
+
 			if(ni.agents.isEmpty()) {
 				continue;
 			}
 
-			try(RemoteShell rsh = makeSsh(ni)) {
-				// TODO: Get agent PIDs 
-				//RemoteActuator.kill(rsh, ni.agents.stream().mapToInt();
-			}
+//			try(RemoteShell rsh = makeSsh(ni)) {
+//				// TODO: Get agent PIDs 
+//				//RemoteActuator.kill(rsh, ni.agents.stream().mapToInt();
+//			}
 		}
 
 		nodes.values().stream()
