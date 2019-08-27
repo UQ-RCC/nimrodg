2ecea2a70c500e7a7862bde67666e1676902479c
Zane van Iperen <z.vaniperen@uq.edu.au>
2019-08-27 11:10:46 +1000

    nimrodg-resources: split NodeInfo and FilterResult into their own files

---
+++ nimrodg-resources/src/main/java/au/edu/uq/rcc/nimrodg/resource/cloud/FilterResult.java
@@ -0,0 +1,74 @@
+package au.edu.uq.rcc.nimrodg.resource.cloud;
+
+import au.edu.uq.rcc.nimrodg.api.utils.NimrodUtils;
+import java.util.ArrayDeque;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+import org.jclouds.compute.domain.NodeMetadata;
+
+class FilterResult {
+
+	public final Map<NodeInfo, Set<UUID>> toLaunch;
+	public final Map<NodeMetadata, Set<UUID>> toFail;
+	public final Set<UUID> leftovers;
+	public final Map<UUID, Integer> indexMap;
+	public final Map<UUID, NodeInfo> uuidMap;
+
+	public FilterResult(Map<NodeInfo, Set<UUID>> toLaunch, Map<NodeMetadata, Set<UUID>> toFail, Set<UUID> leftovers, Map<UUID, Integer> indexMap, Map<UUID, NodeInfo> uuidMap) {
+		this.toLaunch = toLaunch;
+		this.toFail = toFail;
+		this.leftovers = leftovers;
+		this.indexMap = indexMap;
+		this.uuidMap = uuidMap;
+	}
+
+	static FilterResult filterAgentsToNodes(UUID[] uuids, Set<NodeInfo> good, Set<NodeMetadata> bad, int agentsPerNode) {
+		ArrayDeque<NodeInfo> _good = new ArrayDeque<>(good);
+		ArrayDeque<NodeMetadata> _bad = new ArrayDeque<>(bad);
+
+		Map<NodeInfo, Set<UUID>> toLaunch = new HashMap<>(good.size());
+		Map<NodeMetadata, Set<UUID>> toFail = new HashMap<>(bad.size());
+		Map<UUID, NodeInfo> uuidMap = new HashMap<>(uuids.length);
+
+		int i = 0;
+		while(!_good.isEmpty()) {
+			if(i >= uuids.length) {
+				break;
+			}
+
+			NodeInfo ni = _good.peek();
+			Set<UUID> agents = NimrodUtils.getOrAddLazy(toLaunch, ni, nii -> new HashSet<>(agentsPerNode));
+			if(agents.size() >= agentsPerNode) {
+				_good.poll();
+				continue;
+			}
+
+			agents.add(uuids[i]);
+			uuidMap.put(uuids[i], ni);
+			++i;
+		}
+
+		while(!_bad.isEmpty()) {
+			if(i >= uuids.length) {
+				break;
+			}
+
+			NimrodUtils.getOrAddLazy(toFail, _bad.poll(), nn -> new HashSet<>(agentsPerNode)).add(uuids[i++]);
+		}
+
+		Set<UUID> leftovers = new HashSet<>(uuids.length - i);
+		for(int j = i; j < uuids.length; ++j) {
+			leftovers.add(uuids[j]);
+		}
+
+		Map<UUID, Integer> indexMap = new HashMap<>(uuids.length);
+		for(i = 0; i < uuids.length; ++i) {
+			indexMap.put(uuids[i], i);
+		}
+
+		return new FilterResult(toLaunch, toFail, leftovers, indexMap, uuidMap);
+	}
+}
