2b9fa50595cab4a9a73d85049ab4a76946b97954
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-07-29 16:02:23 +1000

    shell/openssh: disable tests on Travis because of https://github.com/UQ-RCC/nimrodg/issues/36

--- nimrodg-shell/src/test/java/au/edu/uq/rcc/nimrodg/shell/ShellTests.java
+++ nimrodg-shell/src/test/java/au/edu/uq/rcc/nimrodg/shell/ShellTests.java
@@ -37,6 +37,7 @@ import java.security.PrivateKey;
 import java.time.Instant;
 import java.util.EnumSet;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
 public class ShellTests {
@@ -64,6 +65,12 @@ public class ShellTests {
 
     @Test
     public void opensshClientTest() throws IOException {
+        String travis = System.getenv("TRAVIS");
+        if(Objects.equals("true", travis)) {
+            System.err.println("Skipping OpenSSH tests on Travis due to https://github.com/UQ-RCC/nimrodg/issues/36");
+            return;
+        }
+
         /* Only test OpenSSH if its available. */
         Path openSsh = Paths.get("/usr/bin/ssh");
         if(!Files.exists(openSsh)) {
