9096580b4f92c4eedcb4eda4d6f4b1b05adbd105
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-08-25 11:31:05 +1000

    shell: add String[] constructor to CommandResult

--- nimrodg-shell/src/main/java/au/edu/uq/rcc/nimrodg/shell/RemoteShell.java
+++ nimrodg-shell/src/main/java/au/edu/uq/rcc/nimrodg/shell/RemoteShell.java
@@ -26,6 +26,7 @@ import java.nio.file.Path;
 import java.nio.file.attribute.PosixFilePermission;
 import java.time.Instant;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -46,6 +47,10 @@ public interface RemoteShell extends Closeable {
 			this.status = status;
 			this.stdout = stdout;
 			this.stderr = stderr;
+		}
+
+		public CommandResult(String[] argv, int status, String stdout, String stderr) {
+			this(ShellUtils.buildEscapedCommandLine(argv), status, stdout, stderr);
 		}
 	}
 
