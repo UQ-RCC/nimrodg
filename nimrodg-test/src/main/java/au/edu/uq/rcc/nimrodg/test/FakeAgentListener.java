ce227e1e7b375246b5604025774a922cdfe19e05
Zane van Iperen <z.vaniperen@uq.edu.au>
2020-09-18 16:30:43 +1000

    test: remove unused import

--- nimrodg-test/src/main/java/au/edu/uq/rcc/nimrodg/test/FakeAgentListener.java
+++ nimrodg-test/src/main/java/au/edu/uq/rcc/nimrodg/test/FakeAgentListener.java
@@ -25,7 +25,6 @@ import au.edu.uq.rcc.nimrodg.agent.DefaultAgentState;
 import au.edu.uq.rcc.nimrodg.agent.ReferenceAgent;
 import au.edu.uq.rcc.nimrodg.agent.messages.AgentMessage;
 import au.edu.uq.rcc.nimrodg.agent.messages.AgentPong;
-import au.edu.uq.rcc.nimrodg.agent.messages.AgentSubmit;
 import au.edu.uq.rcc.nimrodg.agent.messages.AgentUpdate;
 import au.edu.uq.rcc.nimrodg.agent.messages.NetworkJob;
 import au.edu.uq.rcc.nimrodg.api.NimrodMasterAPI;
