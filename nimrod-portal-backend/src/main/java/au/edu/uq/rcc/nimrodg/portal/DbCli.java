4d714ae76a01a462fb6600dd1f71e1ea3c92dea5
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-03-31 15:34:08 +1000

    portal-backend: implement cli argument parsing
    "db init" - (Re)initialise the database.
    "run"     - Run the application server.
    
    Any unrecognized arguments are passed through to Spring.

---
+++ nimrod-portal-backend/src/main/java/au/edu/uq/rcc/nimrodg/portal/DbCli.java
@@ -0,0 +1,78 @@
+/*
+ * Nimrod Portal Backend
+ * https://github.com/UQ-RCC/nimrod-portal-backend
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2020 The University of Queensland
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package au.edu.uq.rcc.nimrodg.portal;
+
+import au.edu.uq.rcc.nimrodg.utils.NimrodUtils;
+import net.sourceforge.argparse4j.inf.Namespace;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.core.env.Environment;
+
+import javax.sql.DataSource;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+
+public class DbCli {
+	private static final String SCHEMA_SQL = NimrodUtils.readEmbeddedFileAsString(
+			PortalServerApplication.class, "schema.sql", StandardCharsets.UTF_8
+	);
+
+	private final Environment env;
+	private final DataSource dataSource;
+
+	private DbCli(Environment env) {
+		this.env = Objects.requireNonNull(env, "environment");
+		this.dataSource = DataSourceBuilder.create()
+				.url(env.getRequiredProperty("spring.datasource.url"))
+				.username(env.getRequiredProperty("spring.datasource.username"))
+				.password(env.getRequiredProperty("spring.datasource.password"))
+				.driverClassName(env.getRequiredProperty("spring.datasource.driver"))
+				.build();
+	}
+
+	public int run(Namespace args, PrintStream out, PrintStream err) {
+		Objects.requireNonNull(args, "args");
+
+		/* For now */
+		if(!"init".equals(args.getString("dbop"))) {
+			throw new IllegalStateException();
+		}
+
+		try(Connection c = dataSource.getConnection()) {
+			c.setAutoCommit(false);
+			try(Statement s = c.createStatement()) {
+				s.executeUpdate(SCHEMA_SQL);
+			}
+			c.commit();
+		} catch(SQLException e) {
+			e.printStackTrace(err);
+			return 1;
+		}
+
+		return 0;
+	}
+
+	public static DbCli withEnvironment(Environment env) {
+		return new DbCli(env);
+	}
+}
--- nimrod-portal-backend/src/main/java/au/edu/uq/rcc/nimrodg/portal/PortalServerApplication.java
+++ nimrod-portal-backend/src/main/java/au/edu/uq/rcc/nimrodg/portal/PortalServerApplication.java
@@ -19,22 +19,91 @@
  */
 package au.edu.uq.rcc.nimrodg.portal;
 
+import net.sourceforge.argparse4j.ArgumentParsers;
+import net.sourceforge.argparse4j.inf.ArgumentParser;
+import net.sourceforge.argparse4j.inf.ArgumentParserException;
+import net.sourceforge.argparse4j.inf.Namespace;
+import net.sourceforge.argparse4j.inf.Subparser;
+import net.sourceforge.argparse4j.inf.Subparsers;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.config.ConfigDataEnvironmentPostProcessor;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.core.env.CommandLinePropertySource;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.SimpleCommandLinePropertySource;
+import org.springframework.core.env.StandardEnvironment;
 
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.List;
 
 @ServletComponentScan
 @SpringBootApplication
 @EnableConfigurationProperties
 @ConfigurationPropertiesScan
 public class PortalServerApplication {
+	public static int cliMain(String[] args) {
+		ArgumentParser parser = ArgumentParsers.newArgumentParser("nimrod-portal-backend")
+				.defaultHelp(true);
+
+		Subparsers aa = parser.addSubparsers().dest("operation");
+
+		Subparser db = aa.addParser("db")
+				.help("Database commands");
+
+		Subparsers dbop = db.addSubparsers().dest("dbop");
+		dbop.addParser("init")
+				.help("Initialise database");
+
+		aa.addParser("run")
+				.help("Run the application server");
+
+		Namespace ns;
+		List<String> springArgs = new ArrayList<>();
+		try {
+			ns = parser.parseKnownArgs(args, springArgs);
+		} catch(ArgumentParserException e) {
+			parser.handleError(e);
+			return 2;
+		}
+
+		String op = ns.getString("operation");
+		if(op == null) {
+			return -1;
+		}
+
+		/*
+		 * NB: This is a bit hacky. Use argparse to handle everything, but since
+		 *  it doesn't support arbitrary --XXX arguments, gather all these "unrecognized" arguments
+		 *  and give them to Spring directly.
+		 */
+		ConfigurableEnvironment env = new StandardEnvironment();
+		env.getPropertySources().addFirst(new SimpleCommandLinePropertySource(springArgs.stream().toArray(String[]::new)));
+		ConfigDataEnvironmentPostProcessor.applyTo(env);
+
+		if("db".equals(op)) {
+			return DbCli.withEnvironment(env).run(ns, System.out, System.err);
+		} else if("run".equals(op)) {
+			SpringApplication sapp = new SpringApplication(PortalServerApplication.class);
+			sapp.setEnvironment(env);
+			sapp.run();
+			return -1;
+		}
+		return 1;
+	}
+
 	public static void main(String[] args) {
 		Security.addProvider(new BouncyCastleProvider());
-		SpringApplication.run(PortalServerApplication.class, args);
+		int r = cliMain(args);
+		if(r >= 0) {
+			System.exit(r);
+		}
+
+		/* Special-case, if cliMain() returns <0, then it's started the application. */
 	}
+
 }
