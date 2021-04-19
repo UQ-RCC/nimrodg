4e0d90f8311f8a3b35023ac58479af92ad6c70a0
Zane van Iperen <z.vaniperen@uq.edu.au>
2021-04-19 16:18:56 +1000

    s/([cC]) 20[[:digit:]][[:digit:]]/(c) 2021/g

--- nimrod-portal-backend/src/main/java/au/edu/uq/rcc/nimrodg/portal/DbCli.java
+++ nimrod-portal-backend/src/main/java/au/edu/uq/rcc/nimrodg/portal/DbCli.java
@@ -3,7 +3,7 @@
  * https://github.com/UQ-RCC/nimrodg
  *
  * SPDX-License-Identifier: Apache-2.0
- * Copyright (c) 2020 The University of Queensland
+ * Copyright (c) 2021 The University of Queensland
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
